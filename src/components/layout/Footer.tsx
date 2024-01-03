892b637c2549350aace9340769ec6107070df022
Jesús Olazagoitia <jesus.olazagoitia@gmail.com>
2024-01-03 08:54:26 +0100

    update footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Footer() {
     return (
         <footer className='text-slate-400 text-sm py-8 px-2 text-center'>
-            Creada por <Link href="https://goiblas.com/">Jesús Olazagoitia</Link>. El código fuente está disponible en <Link href="https://github.com/goiblas/open-data-la-rioja">GitHub</Link>.
+            Un proyecto de <Link href="https://goiblas.com/">Jesús Olazagoitia</Link>. Código disponible en <Link href="https://github.com/goiblas/open-data-la-rioja">GitHub</Link>.
         </footer>
     )
 }
