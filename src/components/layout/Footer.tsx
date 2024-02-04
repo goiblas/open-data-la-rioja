1b5e7742b83b77cb381406a2162bf318bc0ce926
Jesús Olazagoitia <jesus.olazagoitia@gmail.com>
2024-02-04 16:53:06 +0100

    update footer links

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,8 @@ import Link from 'next/link'
 export default function Footer () {
   return (
         <footer className='text-slate-400 text-sm py-8 px-2 text-center'>
-            Un proyecto de <Link href="https://goiblas.com/">Jesús Olazagoitia</Link>. Código disponible en <Link href="https://github.com/goiblas/open-data-la-rioja">GitHub</Link>.
+            Un proyecto de <Link className="underline-offset-2 decoration-slate-600 underline hover:decoration-transparent transition-[text-decoration]" href="https://goiblas.com/">Jesús Olazagoitia</Link>.
+            Código disponible en <Link className="underline-offset-2 decoration-slate-600 underline hover:decoration-transparent transition-[text-decoration]" href="https://github.com/goiblas/open-data-la-rioja">GitHub</Link>.
         </footer>
   )
 }
