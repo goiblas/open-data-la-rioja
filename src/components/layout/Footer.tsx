fc1eeaf3a81523cd75886ef10fbc762c96d92c29
Gabriel Romero <romerogabriel.ad@gmail.com>
2024-03-07 18:46:20 +0100

    Fix commit errors

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,24 +2,24 @@ import Link from 'next/link'
 
 const contributors = [
   {
-    name: "Jesús Olazagoitia",
-    web: "https://goiblas.com/"
+    name: 'Jesús Olazagoitia',
+    web: 'https://goiblas.com/'
   },
   {
-    name: "Gabriel Romero",
-    web: "https://gabrielromero.dev"
+    name: 'Gabriel Romero',
+    web: 'https://gabrielromero.dev'
   }
 ]
 
-export default function Footer() {
+export default function Footer () {
   return (
     <footer className='text-slate-400 text-sm py-8 px-2 text-center'>
-      Un proyecto de 
+      Un proyecto de
       {
         contributors.map(({ name, web }, index) => {
           return (<>
             < Link key={web} className="underline-offset-2 decoration-slate-600 underline hover:decoration-transparent transition-[text-decoration]" href={web}>{name}</Link>
-            {index !== contributors.length - 1 ? " y " : ", "}
+            {index !== contributors.length - 1 ? ' y ' : ', '}
           </>)
         })
       }
