a2a6ddbad350354f39ac2268740574b7babe7270
Jesús Olazagoitia <jesus.olazagoitia@gmail.com>
2024-03-19 20:43:25 +0100

    fix mdx component eslint errors

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,6 +1,6 @@
 import type { MDXComponents } from 'mdx/types'
 
-export function useMDXComponents(components: MDXComponents): MDXComponents {
+export function useMDXComponents (components: MDXComponents): MDXComponents {
   return {
     ...components,
     p: (props) => <p className="text-slate-400 mb-4" {...props} />,
@@ -8,6 +8,6 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     h2: (props) => <h2 className="text-xl text-slate-100 font-bold font-display mb-3 mt-3 md:mt-8" {...props} />,
     h3: (props) => <h3 className="text-lg text-slate-100 font-bold font-display mb-3 mt-3 md:mt-8" {...props} />,
     h4: (props) => <h4 className="text-lg text-slate-100 font-bold font-display mb-3 mt-3 md:mt-8" {...props} />,
-    a: (props) => <a className="text-blue-500 hover:underline" {...props} />,
+    a: (props) => <a className="text-blue-500 hover:underline" {...props} />
   }
 }
