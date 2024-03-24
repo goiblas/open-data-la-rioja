8ef59020df224c4ea536d09ae0c44a1a86f74a60
Jesús Olazagoitia <jesus.olazagoitia@gmail.com>
2024-03-24 21:10:00 +0100

    add logo to not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,6 +5,9 @@ export default function NotFound () {
     <div className='h-[100dvh] grid place-content-center'>
       <div className='text-center p-5'>
         <div className='mb-10'>
+          <Link href="/" className='logo'>
+            <img src="open-data-la-rioja.svg" alt="Open Data La Rioja" className="size-40 md:size-52 mx-auto" />
+          </Link>
           <h2 className='text-3xl text-slate-100 font-bold font-display mb-3'>Página no encontrada</h2>
           <p className="text-lg text-slate-400">La página que estás intentando acceder no está disponible.</p>
         </div>
