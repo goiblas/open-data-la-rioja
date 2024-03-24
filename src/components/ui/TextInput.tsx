1d82dc97897d6c9ecb037efb756ad7b18b8b6549
Jesús Olazagoitia <jesus.olazagoitia@gmail.com>
2024-03-24 10:57:04 +0100

    add className to textInput

--- src/components/ui/TextInput.tsx
+++ src/components/ui/TextInput.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import clsx from 'clsx'
 type TextInputProps = React.InputHTMLAttributes<HTMLInputElement>
 
 export default function TextInput (props: TextInputProps) {
@@ -7,11 +7,7 @@ export default function TextInput (props: TextInputProps) {
 
   return (
         <input
-            className="
-            outline-none
-            transition duration-150 ease-in-out
-            border border-slate-300 text-md rounded-lg block w-full p-3 bg-slate-700 dark:border-slate-600 placeholder-slate-400
-            text-slate-50 focus:ring-blue-500 focus:border-blue-500"
+            className={clsx(className, 'outline-none transition duration-150 ease-in-out border border-slate-300 text-md rounded-lg block w-full p-3 bg-slate-700 dark:border-slate-600 placeholder-slate-400 text-slate-50 focus:ring-blue-500 focus:border-blue-500')}
             type={type}
             {...rest}
         />
