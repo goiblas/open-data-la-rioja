262dd4f33c79f04960192beb492e5df4ccc9b3c2
Jesús Olazagoitia <jesus.olazagoitia@gmail.com>
2024-04-01 19:30:09 +0200

    udpate footer with contributors

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,9 +2,18 @@ import Link from 'next/link'
 
 export default function Footer () {
   return (
-    <footer className='text-slate-400 text-sm py-8 px-2 text-center'>
-      Un proyecto de <Link className="underline-offset-2 decoration-slate-600 underline hover:decoration-transparent transition-[text-decoration]" href="https://goiblas.com/">Jesús Olazagoitia</Link>,
-      código disponible en <Link className="underline-offset-2 decoration-slate-600 underline hover:decoration-transparent transition-[text-decoration]" href="https://github.com/goiblas/open-data-la-rioja" > GitHub</Link >.
+    <footer className='text-slate-300 text-xs py-8 px-2 w-full max-w-[900px] mx-auto text-center md:text-left flex flex-col md:flex-row justify-between gap-4'>
+      <div>
+        <div className='mb-1'>Desarrollado con ♥ por:</div>
+        <div>
+          <Link className="underline-offset-2 decoration-slate-600 underline hover:decoration-transparent" href="https://goiblas.com/">Jesús Olazagoitia</Link>, {' '}
+          <Link className="underline-offset-2 decoration-slate-600 underline hover:decoration-transparent" href="https://www.gabrielromero.dev/">Gabriel Romero</Link>
+        </div>
+      </div>
+
+      <div>
+        Código disponible en <Link className="underline-offset-2 decoration-slate-600 underline hover:decoration-transparent" href="https://github.com/goiblas/open-data-la-rioja">GitHub</Link>
+      </div>
     </footer >
   )
 }
