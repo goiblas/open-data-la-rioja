69607f9378e1a3961a5ff3d35433f01be98fe3b6
Jesús Olazagoitia <jesus.olazagoitia@gmail.com>
2024-04-01 10:22:04 +0200

    add key to origin urls

--- src/components/ui/CardExpanded.tsx
+++ src/components/ui/CardExpanded.tsx
@@ -1,4 +1,5 @@
 import { Card } from '@tremor/react'
+import { Fragment } from 'react'
 
 interface CardExpandedProps {
   children: React.ReactNode
@@ -29,13 +30,13 @@ export default function CardExpanded (props: CardExpandedProps) {
                 <div className='py-4'>
                     <p className="text-center text-xs text-gray-400">Fuentes: {''}
                         {originUrls.map((url, index) => (
-                            <>
-                            <a key={index} href={url} className="underline-offset-2 decoration-slate-600 underline hover:decoration-transparent transition-[text-decoration]">
-                                {url}
-                            </a>
-                            {index < originUrls.length - 2 && ', '}
-                            {index === originUrls.length - 2 && ' y '}
-                            </>
+                            <Fragment key={url}>
+                                <a href={url} className="underline-offset-2 decoration-slate-600 underline hover:decoration-transparent transition-[text-decoration]">
+                                    {url}
+                                </a>
+                                {index < originUrls.length - 2 && ', '}
+                                {index === originUrls.length - 2 && ' y '}
+                            </Fragment>
                         ))}
                     </p>
                 </div>
