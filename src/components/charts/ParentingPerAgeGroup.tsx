e808dc533d0953ec44af88b255d7cd71e8009155
ibaialberdi <ibaialberdi@gmail.com>
2024-04-07 16:27:20 +0200

    Added filtering by sex to ParentingPerAgeGroup

--- src/components/charts/ParentingPerAgeGroup.tsx
+++ src/components/charts/ParentingPerAgeGroup.tsx
@@ -5,12 +5,16 @@ import CardExpanded from '@/components/ui/CardExpanded'
 export default async function ParentingPerAgeGroup ({ originUrl }: { originUrl: string }) {
   const { index, data, categories } = await getParentingPerAgeGroup()
 
+  const groupNames = ['Mujeres', 'Hombres', 'Ambos']
+
   return (
         <CardExpanded originUrl={originUrl}>
             <ParentingPerAgeGroupClient
                 data={data}
                 index={index}
                 categories={categories}
+                groupNames={groupNames}
+                label='Sexo'
             />
         </CardExpanded>
   )
--- src/components/charts/ParentingPerAgeGroupClient.tsx
+++ src/components/charts/ParentingPerAgeGroupClient.tsx
@@ -1,25 +1,48 @@
 'use client'
-import { AreaChart, type AreaChartProps } from '@tremor/react'
+import { AreaChart, Select, type AreaChartProps, SelectItem } from '@tremor/react'
 import { formatLargeNumber } from '@/lib/shared/formatters'
+import { useState } from 'react'
 
 interface ParentingPerAgeGroupClientProps {
   data: AreaChartProps['data']
   index: AreaChartProps['index']
   categories: AreaChartProps['categories']
+  groupNames: string[]
+  label: string
 }
 
 export default function ParentingPerAgeGroupClient (props: ParentingPerAgeGroupClientProps) {
-  const { data, index, categories } = props
+  const { data, index, categories, groupNames, label } = props
+
+  const [groupSelected, setGroupSelected] = useState<string>(groupNames.at(-1))
+
+  const filteredCategories = groupSelected === 'Ambos' ? categories : categories.filter(c => c.startsWith(groupSelected))
 
   return (
-        <AreaChart
+    <div className="mt-6">
+      <div className="flex justify-end items-center gap-2">
+        <div className='text-slate-500 font-bold text-sm'>
+          {label}:
+        </div>
+        <div>
+          <Select className='max-w-44' enableClear={false} value={groupSelected} onValueChange={setGroupSelected}>
+            {groupNames.map((groupName) => (
+              <SelectItem key={groupName} value={groupName}>{groupName}</SelectItem>
+            ))}
+          </Select>
+        </div>
+      </div>
+
+      <AreaChart
             data={data}
             index={index}
-            categories={categories}
+            categories={filteredCategories}
             valueFormatter={formatLargeNumber}
             onValueChange={() => {}}
             yAxisWidth={32}
             connectNulls
         />
+    </div>
+
   )
 }
